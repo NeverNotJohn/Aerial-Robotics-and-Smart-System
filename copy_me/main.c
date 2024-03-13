25b63a785958268a815e71f91fbcd6757b5d90af
John <johntheherrera@gmail.com>
2024-03-12 18:05:00 -0700

    added copy_me template

---
+++ copy_me/main.c
@@ -0,0 +1,13 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "queue.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+
+int main() {
+    // Put your magic code here!
+}
